fb9aeb57525b712428637167be1e482d29f2c2c1
Sukritee Sharma <87569188+aceta-minophen@users.noreply.github.com>
2023-08-25 13:51:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import cv2
+import threading
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+# Initialize the USB camera
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Encode the frame in JPEG format
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                continue
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
